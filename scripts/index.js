bcaa0150f95394dcaf13487909cfb87914b70723
alexisdev <alexisescobardev@gmail.com>
2024-09-28 09:57:03 -0300

    agregando swipper

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
