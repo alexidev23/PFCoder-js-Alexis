ad575c0e095b065aed51d74326f422c9f7cbb54e
AlexisDev <alexisescobar1234@gmail.com>
2024-10-21 09:32:23 -0300

    agregando funcionalidad para el boton login y carrito de compra

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,30 @@
-// const nombre = prompt('Ingrese su nombre: ')
+// Iniciar Sesion
+const logIn = document.getElementById('usuario');
 
-// alert(`Hola bienvenido,${nombre}`)
-// alert(`${nombre} quiere seguir con su compra?`)
-// let compra = prompt('(1:SI, 2:NO): ')
+document.querySelector('#logInUser').addEventListener('click', () => {
+  newUser()
+})
 
-// function confirmarPedido(compra) {
-//   if (compra === '2' || compra === 'NO' || compra === 'no') {
-//     alert('Gracias por su visita!')
-//   } else if (compra === '1' || compra === 'SI' || compra === 'si') {
-//     alert('Este es su carrito:\n 1\tPc AMD Ryzen 7 5700G 8Gb SSD 240Gb + Monitor 22\n 1\tCelular Motorola Edge 40 6.55" 8/256GB magenta')
-//     alert('Necesitamos los siguientes datos para completar el pedido:\n- Apellido,\n- Celular,\n- Correo Electronico,\n- Direccion,\n- Codigo Postal')
-//     const apellido = prompt('Ingrese su apellido:')
-//     const celular = prompt('Ingrese su número de celular:')
-//     const correo = prompt('Ingrese su correo electrónico:')
-//     const direccion = prompt('Ingrese su dirección:')
-//     const codigo = prompt('Ingrese su código postal:')
-  
-//     alert('Gracias por su compra, una vez despachada su compra le va a llegar un correo sobre el dia y hora que va a estar llegando su pedido en el domicilio establecido')
-//   }
-// }
+const newUser = () => {
+  alert('Bienvenido a Tech Shop, por favor a continuacion ingrese su usuario y contraseña')
+  const usuario = prompt('Ingrese su usuario:')
+  const password = prompt('Ingrese su contraseña:')
 
-// confirmarPedido(compra)
+  if (usuario === '' && password === '' || usuario === '' && password !== '' || usuario !== '' && password === '' || usuario === null && password === null) {
+    alert('Por favor ingrese los datos necesarios para iniciar sesion')
+  } else if (usuario !== '' && password !== ''){
+    logIn.innerHTML = `<p class='login'>Bienvenida/o, ${usuario}!</p>`
+    document.querySelector('.cart-count').textContent = 2;
+  }
+}
 
-// Scripts para el header
+// Scripts para el el boton de las categorias
 
 document.querySelector('.categories-btn').addEventListener('click', function() {
   document.querySelector('.categories-content').classList.toggle('show');
 });
 
-// Cerrar el menú desplegable si el usuario hace clic fuera de él
+// Cerrar el menú desplegable de categorias si el usuario hace clic fuera de él
 window.onclick = function(event) {
   if (!event.target.matches('.categories-btn')) {
       var dropdowns = document.getElementsByClassName("categories-content");
@@ -41,24 +37,24 @@ window.onclick = function(event) {
   }
 }
 
-// Simulación de agregar al carrito
-let cartCount = 0;
-document.querySelector('.cart-icon').addEventListener('click', function() {
-  cartCount++;
-  document.querySelector('.cart-count').textContent = cartCount;
-});
-
 // Scripts para el carrusel de la publicidad
 const swiper = new Swiper('.swiper', {
-
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
   // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
   },
 });
+
+// confirmarPedido(compra)
+// Mostrando la cantidad de productos que tiene en el carrito de compra
+// Simulación de agregar al carrito
+// let cartCount = 0;
+// document.querySelector('.cart-icon').addEventListener('click', function() {
+//   cartCount++;
+//   document.querySelector('.cart-count').textContent = 4;
+// });
