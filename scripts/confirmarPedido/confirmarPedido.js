9a45d43e8cd8cebd4928cdfaf0aea2f5651beb3b
AlexisDev <alexisescobar1234@gmail.com>
2024-11-21 17:48:05 -0300

    validacion de formulario de confirmacion de compra y componente de confirmacion de compra

--- scripts/confirmarPedido/confirmarPedido.js
+++ scripts/confirmarPedido/confirmarPedido.js
@@ -1,14 +1,43 @@
-const form = document.getElementById('confirmarPedido');
+const form = document.getElementById('confirmarPedido')
+const modal = document.querySelector('.modal')
 
 if (form) {
   form.addEventListener('submit', (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    alert('Pedido realizado con exito\nDentro de unos dias le va a llegar un correo con un link para seguir el envio')
+    // Teléfono
+    const tel = document.getElementById('tel').value.trim();
+    const errorTel = document.querySelector('.errorTel');
+    errorTel.innerHTML = ``
 
+    if (tel.length > 15 || tel.length < 12) {
+      errorTel.innerHTML = `<span>El número de teléfono debe tener entre 12 y 15 dígitos.</span>`
+      return
+    }
+
+    // Código Postal
+    const CP = document.getElementById('codigoPostal').value.trim()
+    const errorCP = document.querySelector('.errorCP')
+    errorCP.innerHTML = ``
+
+    if (CP.length > 5 || CP.length < 4) {
+      errorCP.innerHTML = `<span>El código postal debe tener entre 4 y 5 dígitos.</span>`
+      return
+    }
+
+    // Número de casa
+    const numCasa = document.getElementById('numCalle').value.trim()
+    const errorNumCasa = document.querySelector('.errorNumCasa')
+    errorNumCasa.innerHTML = ``
+
+    if (numCasa.length > 4 || numCasa.length < 1) {
+      errorNumCasa.innerHTML = `<span>El número de casa debe tener entre 1 y 4 dígitos.</span>`
+      return
+    }
+
+    // Si todas las validaciones pasan
+    modal.style.display = "flex"
     localStorage.removeItem('carrito')
-
-    // Redirigir a otra página
-    window.location.href = './carrito.html';
+    
   });
 }
